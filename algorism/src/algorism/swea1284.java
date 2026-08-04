f005310418c373ae6134277cc261dcb620aa9319
bagjiwon0 <bagjiwon0@gmail.com>
2026-08-04 17:55:19 +0900

    algo

--- algorism/src/algorism/swea1284.java
+++ algorism/src/algorism/swea1284.java
@@ -11,6 +11,38 @@ public class swea1284 {
 		
 		for(int TEST_CASE = 1; TEST_CASE <= T; TEST_CASE++) {
 			
+			int normalLiterPerMoney = sc.nextInt(); // 1리터당 P원
+			int normalMoney = sc.nextInt();			// 기본요즘 Q원
+			int liter = sc.nextInt();				// 월간사용량 R리터이하
+			int literMoney = sc.nextInt();			// R보다 많은경우 초과량에 대해 1L당 S원
+			int userLiter = sc.nextInt();			// 내가 사용한 수도 양 W리터
+			
+			
+			
+			int AMoney = normalLiterPerMoney * userLiter;
+			int BMoney = 0;
+			
+			if(userLiter <= liter) {
+				BMoney = normalMoney;
+			}
+			else if(userLiter > liter) {
+				BMoney = normalMoney + literMoney * (userLiter - liter);
+				}
+			
+			int result = 0;
+			if(AMoney >= BMoney) {
+				result = AMoney;
+			}
+			else if(AMoney < BMoney) {
+				result = BMoney;
+			}
+			
+			System.out.println("#" + TEST_CASE + " " + result);
+			
+			
+			
+			
+			
 		}
 
 	}
--- algorism/src/algorism/swea1288.java
+++ algorism/src/algorism/swea1288.java
@@ -4,17 +4,42 @@ import java.util.*;
 public class swea1288 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+		int T = sc.nextInt();		
 		
-		
-		
-		
-		for(int TEST_CASE = 1; TEST_CASE <= T; TEST_CASE++) {
-			int Num = sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++) {
 			
+			int num = sc.nextInt();
+			int[] arr = new int[10];
+			int count = 1;
 			
-			
-			
+			while(true) {
+				
+				int mul = num * count;
+				int copy = mul;
+				
+				while(copy > 0) {
+					arr[copy % 10]++;
+					copy /= 10;
+				}
+				
+				boolean isOk = true;
+				
+				for(int i = 0; i < 10; i++) {
+					if(arr[i] == 0) {
+						isOk = false;
+						break;
+					}
+				}
+				
+				if(isOk) {
+					System.out.println("#" + test_case + " " + count);
+					break;
+				}
+				
+				count++;			
+
+			}
+
 		}
 		
 		
