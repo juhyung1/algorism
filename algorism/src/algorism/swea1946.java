79531102ffd17f8e00565255d82e892fabe7cb71
ljh <juhyung1633@gmail.com>
2026-08-07 00:21:52 +0900

    0806 home

---
+++ algorism/src/algorism/swea1946.java
@@ -0,0 +1,17 @@
+package algorism;
+import java.util.*;
+
+public class swea1946 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			
+		}// tc
+	}// main
+
+}
