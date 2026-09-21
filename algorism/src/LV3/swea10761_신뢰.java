c1bd4f8d414dea4af3d592eaec1d08382cfbc131
임주형 <juhyung1633@gmail.com>
2026-09-21 18:00:19 +0900

    0921

---
+++ algorism/src/LV3/swea10761_신뢰.java
@@ -0,0 +1,36 @@
+package LV3;
+
+import java.util.Scanner;
+
+public class swea10761_신뢰 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+			
+			int time = 0;
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}	// tc
+	}		// main
+}
